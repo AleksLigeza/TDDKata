fd9f7c57091e3d7a5f85d969b45b4e84d83e91b6
Saxtonn <aleksligeza@gmail.com>
2017-10-09 17:01:30 +0200

    Added BowlingGame kata

--- TDDKata.Tests/BowlingGameTest.cs
+++ TDDKata.Tests/BowlingGameTest.cs
@@ -2,12 +2,68 @@
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
+using CodeKata;
 
-namespace TDDKata.Tests
-{
+namespace TDDKata.Tests {
     [TestFixture]
-    class BowlingGameTest
-    {
+    class BowlingGameTest {
 
+        #region privateFields
+        private BowlingGame _game;
+        private int _frames = 10;
+        #endregion
+
+        #region Tests
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [Test]
+        public void CanHandleSimilarLowScoreGame(int score) {
+            _game = new BowlingGame();
+            RollManySameScores(score, _frames * 2);
+            Assert.AreEqual(score * _frames * 2, _game.Score());
+        }
+
+        [Test]
+        public void CanHandleSpares() {
+            _game = new BowlingGame();
+            RollSpare();
+            _game.Roll(3);
+            RollManySameScores(0, 17);
+            Assert.AreEqual(16, _game.Score());
+        }
+
+        [Test]
+        public void CanHandleStrike() {
+            _game = new BowlingGame();
+            _game.Roll(10);
+            _game.Roll(3);
+            _game.Roll(4);
+            RollManySameScores(16, 0);
+            Assert.AreEqual(24, _game.Score());
+        }
+
+        [Test]
+        public void CanHandlePerfectGame() {
+            _game = new BowlingGame();
+            RollManySameScores(10, 12);
+            Assert.AreEqual(300, _game.Score());
+        }
+        #endregion
+
+        #region TestHelpers
+
+        public void RollManySameScores(int score, int rollsCount) {
+            for (int i = 0; i < rollsCount; i++) {
+                _game.Roll(score);
+            }
+        }
+
+        public void RollSpare() {
+            _game.Roll(5);
+            _game.Roll(5);
+        }
+
+        #endregion
     }
 }
--- TDDKata/BowlingGame.cs
+++ TDDKata/BowlingGame.cs
@@ -2,9 +2,52 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CodeKata
-{
-    class BowlingGame
-    {
+namespace CodeKata {
+    public class BowlingGame {
+        private int[] _rolls = new int[21];
+        private int _currentRoll = 0;
+
+        public void Roll(int pins) {
+            _rolls[_currentRoll] = pins;
+            _currentRoll++;
+        }
+
+        public int Score() {
+            int score = 0;
+            int roll = 0;
+
+            for (int frame = 0; frame < 10; frame++) {
+                int frameScore = GetFrameScore(roll);
+                score += frameScore;
+                if (frameScore == 10) {
+                    if (_rolls[roll] == 10) {
+                        score += GetStrikeBonus(roll);
+                        roll++;
+                    } else {
+                        score += GetSpareBonus(roll);
+                        roll += 2;
+                    }
+                } else {
+                    roll += 2;
+                }
+            }
+
+            return score;
+        }
+
+        public int GetFrameScore(int roll) {
+            if (_rolls[roll] != 10) {
+                return _rolls[roll] + _rolls[roll + 1];
+            }
+            return 10;
+        }
+
+        public int GetStrikeBonus(int roll) {
+            return _rolls[roll + 1] + _rolls[roll + 2];
+        }
+
+        public int GetSpareBonus(int roll) {
+            return _rolls[roll + 2];
+        }
     }
 }
