fa4270cf99b1e6f15d56fdc43d636c5f0331c11b
Saxtonn <aleksligeza@gmail.com>
2017-10-09 17:39:44 +0200

    Added FizzBuzz kata, without if statement

---
+++ TDDKata.Tests/FizzBuzzTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+[TestFixture]
+class FizzBuzzTest {
+
+    #region PrivateFields
+    private FizzBuzz _fizzBuzz;
+    #endregion
+
+    #region Congig
+    [OneTimeSetUp]
+    public void StartUp() {
+        _fizzBuzz = new FizzBuzz();
+    }
+
+    [OneTimeTearDown]
+    public void TearDown() {
+        _fizzBuzz = null;
+    }
+    #endregion
+
+    #region TestMethods
+    [TestCase(3)]
+    [TestCase(-9)]
+    [TestCase(0)]
+    [Test]
+    public void CanReturnFizzIfNumberIsMultipleByThree(int number) {
+        Assert.True(_fizzBuzz.PrintNumber(number).Contains("Fizz"));
+    }
+
+    [TestCase(5)]
+    [TestCase(-10)]
+    [TestCase(0)]
+    [Test]
+    public void CanReturnBuzzIfNumberIsMultipleByFive(int number) {
+        Assert.True(_fizzBuzz.PrintNumber(number).Contains("Buzz"));
+    }
+
+    [TestCase(5)]
+    [TestCase(-10)]
+    [TestCase(13)]
+    [Test]
+    public void DontReturnFizzIfNumberIsNotMultipleByThree(int number) {
+        Assert.False(_fizzBuzz.PrintNumber(number).Contains("Fizz"));
+    }
+
+    [TestCase(3)]
+    [TestCase(-12)]
+    [TestCase(13)]
+    [Test]
+    public void DontRetunrBuzzIfNumberIsNotMultipleByFive(int number) {
+        Assert.False(_fizzBuzz.PrintNumber(number).Contains("Buzz"));
+    }
+
+    [Test]
+    public void CanReturnNumbersUsingLinq() {
+        Assert.AreEqual(_fizzBuzz.PrintNumbers(), _fizzBuzz.PrintNumbersUsingLinq());
+    }
+#endregion
+}
+
---
+++ TDDKata/FizzBuzzWithoutUsingIf.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+public class FizzBuzz {
+
+    public string PrintNumbers() {
+        string value = "";
+        for (int i = 0; i < 100; i++) {
+            value += PrintNumber(i) + "\n";
+        }
+        Console.Write(value);
+
+        return value;
+    }
+
+    public string PrintNumber(int number) {
+        string value = number.ToString();
+        value = IsFizz(number) ? "Fizz" : value;
+        value = IsBuzz(number) ? "Buzz" : value;
+        value = IsFizzBuzz(number) ? "FizzBuzz" : value;
+
+        return value;
+    }
+
+    public bool IsFizz(int number) {
+        return number % 3 == 0;
+    }
+
+    public bool IsBuzz(int number) {
+        return number % 5 == 0;
+    }
+
+    public bool IsFizzBuzz(int number) {
+        return (IsFizz(number) && IsBuzz(number)) ? true : false;
+    }
+
+    public string PrintNumbersUsingLinq() {
+        string value = "";
+        Enumerable.Range(0, 100)
+            .Select(n => IsFizzBuzz(n) ? "FizzBuzz" : IsFizz(n) ? "Fizz" : IsBuzz(n) ? "Buzz" :  n.ToString())
+            .ToList()
+            .ForEach(n => value += n + "\n");
+        return value;
+    }
+}
